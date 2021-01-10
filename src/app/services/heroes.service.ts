e969b88354bd0a7425e6c925bd0ea6a69a5dfe7c
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2021-01-09 22:48:28 -0600

    Aplicacion finalizada

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -38,7 +38,8 @@ export class HeroesService {
   getHeroes() {
     return this.http.get(`${ this.url }/Heroes.json`)
       .pipe(
-        map( this.crearArreglo )
+        map( this.crearArreglo ),
+        delay(1500)
       );
   }
 
